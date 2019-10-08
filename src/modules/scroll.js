08be78259d7ec7a2b236096e71f269f764792596
Nata Krass <natalikrasnobaieva@gmail.com>
2019-10-08 20:00:27 +0300

    arrow

---
+++ src/modules/scroll.js
@@ -0,0 +1,19 @@
+const scroll = () => {
+    const arrow = document.getElementById('totop');
+
+    const show = () => {
+        arrow.style.display = 'block'; 
+    }
+    const close = () => {
+        arrow.style.display = 'none'; 
+    }
+    arrow.style.display = 'none';
+    window.addEventListener('scroll', function() {
+        if(pageYOffset > 600) {
+            show();
+        } else {
+            close();
+        }
+    });
+}
+scroll();
